f62e804f26198bc9e6b7d210f0d4c3cacd949eec
AironasGit <aironaxas@gmail.com>
2024-05-24 23:05:53 +0300

    Introduced settings window

--- main.py
+++ main.py
@@ -110,13 +110,13 @@ class AppDirs:
         self.user_docs_dir: str = os.path.expanduser('~\\Documents')
         self.app_dir = self.user_docs_dir + "\\Imager"
         self.images_dir = self.app_dir + "\\Images"
-        self.temp_dir = self.app_dir + "\\Temp"
+        self.settings_dir = self.app_dir + "\\Settings"
         self.__check_dirs()
     
     def __check_dirs(self):
         self.__check_dir(self.app_dir)
         self.__check_dir(self.images_dir)
-        self.__check_dir(self.temp_dir)
+        self.__check_dir(self.settings_dir)
         
     def __check_dir(self, dir):
         if not os.path.exists(dir):
@@ -298,22 +298,107 @@ class RegisterWindow:
 
     def __register_button(self):
         self.user.register(self.username, self.password, self.confirm_password)
+ 
+class SettingsWindow:
+    # Settings:
+    # 1. Store locally
+    # 2. Store in DB (for website) (only available if logged in)
+    # 3. Editable keybinds for taking screenshots
+    # 4. Stay logged in
+    settings: configparser.ConfigParser = configparser.ConfigParser()
+    settings_path = f'{AppDirs().settings_dir}\\settings.ini'
+    def __init__(self):
+        self.__generate_default_settings()
+        self.__init_settings()
+        self.__init_ui()
+        
+    def __init_ui(self):
+        ratio = 50
+        self.window = tk.Tk()
+        self.window.resizable(False, False)
+        width, height = 16*ratio, 9*ratio
+        x = (self.window.winfo_screenwidth()//2)-(width//2)
+        y = (self.window.winfo_screenheight()//2)-(height//2)
+        self.window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        canvas = tk.Canvas(self.window, width=width, height=height, borderwidth=0, highlightthickness=0)
+        canvas.pack(expand=True)
+        
+        check_button_local_var = tk.IntVar(value=self.storage_local)
+        check_button_local = tk.Checkbutton(canvas, text='Store locally',variable=check_button_local_var, onvalue=1, offvalue=0, command= lambda: self.__change_setting('Storage', 'Local', str(check_button_local_var.get()))).place(x=50, y=50)
+        
+        check_button_db_var = tk.IntVar(value=self.storage_db)
+        check_button_db = tk.Checkbutton(canvas, text='Store in Database',variable=check_button_db_var, onvalue=1, offvalue=0, command= lambda: self.__change_setting('Storage', 'Db', str(check_button_db_var.get()))).place(x=50, y=70)
+        
+        self.screen_shot_var = tk.StringVar(value=self.screen_shot)
+        screen_shot_label = tk.Label(canvas, text='Keybind to screen shot').place(x=180, y=95)
+        screen_shot_entry = tk.Entry(canvas, textvariable=self.screen_shot_var).place(x=55, y=95)
+        
+        self.screen_shot_edit_var = tk.StringVar(value=self.screen_shot_edit)
+        screen_shot_edit_label = tk.Label(canvas, text='Keybind to screen shot and edit').place(x=180, y=120)
+        screen_shot_edit_entry = tk.Entry(canvas, textvariable=self.screen_shot_edit_var).place(x=55, y=120)
+        
+        self.settings_window_var = tk.StringVar(value=self.settings_window)
+        settings_window_label = tk.Label(canvas, text='Keybind to open settings window').place(x=180, y=145)
+        settings_window_entry = tk.Entry(canvas, textvariable=self.settings_window_var).place(x=55, y=145)
+        
+        save_button = tk.Button(canvas, text='Save changes', command=self.__save_keybind_changes).place(x=50, y=300)
+        
+        self.window.mainloop()
+        
+    def __save_keybind_changes(self):
+        keybind_changes = [
+            ('KeyBinds', 'settings_window', self.settings_window_var.get()),
+            ('KeyBinds', 'screen_shot', self.screen_shot_var.get()),
+            ('KeyBinds', 'screen_shot_edit', self.screen_shot_edit_var.get())
+        ]
+        for keybind_change in keybind_changes:
+            self.__change_setting(keybind_change[0], keybind_change[1], keybind_change[2])
+    
+    def __change_setting(self, section, key, value):
+        self.settings.set(section, key, value)
+        with open(self.settings_path, 'w') as f:
+            self.settings.write(f)
+        self.__init_settings()
+    
+    def __init_settings(self):
+        self.storage_local: int = self.settings.getint('Storage', 'local')
+        self.storage_db: int = self.settings.getint('Storage', 'db')
+        self.screen_shot: str = self.settings.get('KeyBinds', 'screen_shot')
+        self.screen_shot_edit: str = self.settings.get('KeyBinds', 'screen_shot_edit')
+        self.settings_window: str = self.settings.get('KeyBinds', 'settings_window')
+    
+    def __generate_default_settings(self):
+        if not os.path.exists(self.settings_path):
+            self.settings = configparser.ConfigParser()
+            self.settings['Storage'] = {'local': '1',
+                                        'db': '0'}
+            self.settings['KeyBinds'] = {'screen_shot': 'ctrl+1',
+                                        'screen_shot_edit': 'ctrl+2',
+                                        'settings_window': 'ctrl+3'}
+            with open(self.settings_path, 'w') as settingsfile:
+                self.settings.write(settingsfile)
+            self.settings.read(self.settings_path)
+        else:
+            self.settings.read(self.settings_path)
     
 def main():
     login_window = LoginWindow()
+    app_dirs = AppDirs()
+    settings = SettingsWindow()
     while True:
         if login_window.user.logged_in:
-            app_dirs = AppDirs()
-            if keyboard.is_pressed('1'): # Full screen shot
+            if keyboard.is_pressed(settings.screen_shot): # Full screen shot
                 screen = ImageGrab.grab()
                 image_name = f'\\{int(time.time())}.jpg'
                 image_path = app_dirs.images_dir + image_name 
                 screen.save(image_path)
-            if keyboard.is_pressed('2'): # Select a rectangle inside screen shot
+            if keyboard.is_pressed(settings.screen_shot_edit): # Select a rectangle inside screen shot
                 screen = ImageGrab.grab()
                 image_name = f'\\{int(time.time())}.jpg'
                 image_path = app_dirs.images_dir + image_name
                 EditableImageWindow(screen, image_path)
+            if keyboard.is_pressed(settings.settings_window): # Settings window
+                settings = SettingsWindow()
         else:
             break
     #app_dirs = AppDirs()
