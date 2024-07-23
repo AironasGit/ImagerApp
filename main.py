278f1728933c4ae2b0bc4e7285f6f4da2a066b32
AironasGit <aironaxas@gmail.com>
2024-07-23 18:20:37 +0300

    refactoring

--- main.py
+++ main.py
@@ -25,24 +25,30 @@ class API:
         
     def login(self, username: str, password: str) -> int:
         data = {'username': username, 'password': password}
-        r = requests.post(url = self.validate_user_url, data=data)
-        return r.status_code
+        response = requests.post(url = self.validate_user_url, data=data)
+        return response
     
     def upload_img(self, username: str, password: str, is_private: bool, description: str, image) -> int:
         data = {'username': username, 'password': password, 'is_private': is_private, 'description': description}
-        r = requests.post(url = self.upload_img_url, data=data, files={'image': image})
-        return r.status_code
+        response = requests.post(url = self.upload_img_url, data=data, files={'image': image})
+        return response
         
 class DrawRectangle:
-    id: int = None
-    def __init__(self, window: tk.Tk, canvas:tk.Canvas, image: Image):
-        self.window = window
-        self.image: Image = image
+    id: int
+    image: Image
+    canvas: tk.Canvas
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    def __init__(self, canvas:tk.Canvas, image: Image):
+        self.image = image
         self.canvas = canvas
-        self.x1: int = 0
-        self.y1: int = 0
-        self.x2: int = 0
-        self.y2: int = 0
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+        self.id = None
         
     def update_start_point(self, mouse: tk.Event):
         self.x1 = mouse.x
@@ -61,11 +67,15 @@ class DrawRectangle:
         self.canvas.coords(self.id, self.x1, self.y1, self.x2, self.y2)
       
 class AppDirs:
+    user_docs_dir: str
+    app_dir: str
+    images_dir: str
+    settings_dir: str
     def __init__(self):
-        self.user_docs_dir: str = os.path.expanduser('~\\Documents')
-        self.app_dir = self.user_docs_dir + "\\Imager"
-        self.images_dir = self.app_dir + "\\Images"
-        self.settings_dir = self.app_dir + "\\Settings"
+        self.user_docs_dir = os.path.expanduser('~/Documents')
+        self.app_dir = self.user_docs_dir + "/Imager"
+        self.images_dir = self.app_dir + "/Images"
+        self.settings_dir = self.app_dir + "/Settings"
         self.__check_dirs()
     
     def __check_dirs(self):
@@ -78,18 +88,20 @@ class AppDirs:
             os.mkdir(dir)
             
 class User:
-    logged_in: bool = False
-    username: str = None
-    _password: str = None
+    logged_in: bool
+    username: str
+    _password: str
     def __init__(self):
-        pass
+        self.logged_in = False
+        self.username = None
+        self._password = None
         
     def login(self, username: tk.StringVar, password: tk.StringVar):
         api = API()
         self.username = username.get()
         self._password = password.get()
-        r_code =api.login(self.username, self._password)
-        match r_code:
+        response: requests.Response = api.login(self.username, self._password)
+        match response.status_code:
             case 200:
                 self.logged_in = True
                 popup_message("Login successful!")
@@ -99,8 +111,10 @@ class User:
                 popup_message("Something is wrong")
         
 class EditableImageWindow:
+    image: Image
+    image_path: str
     def __init__(self, image: Image, image_path: str):
-        self.image: Image = image
+        self.image = image
         self.image_path = image_path
         self.__init_ui()
     
@@ -114,13 +128,12 @@ class EditableImageWindow:
         image = ImageTk.PhotoImage(self.image)
         canvas.create_image(0, 0, image=image, anchor=tk.NW)
         
-        self.rect = DrawRectangle(self.window, canvas, self.image)
+        self.rect = DrawRectangle(canvas, self.image)
         self.rect.id = canvas.create_rectangle(self.rect.x1, self.rect.y1, self.rect.x2, self.rect.y2, dash=(2,2), fill='', outline='red')
 
         canvas.bind('<Button-1>', lambda event: self.__button1(event))
         canvas.bind('<B1-Motion>', lambda event: self.__button1_motion(event))
         canvas.bind('<ButtonRelease-1>', lambda event: self.__button1_release())
-        #canvas.bind('<Escape>', lambda event: self.window.destroy())
 
         self.window.mainloop()
     
@@ -138,8 +151,9 @@ class EditableImageWindow:
 class LoginWindow:
     username: tk.StringVar
     _password: tk.StringVar
-    user: User = User()
+    user: User
     def __init__(self):
+        self.user = User()
         self.__init_ui()
     
     def __init_ui(self):
@@ -173,14 +187,17 @@ class LoginWindow:
             self.window.destroy()
     
 class SettingsWindow:
-    # Settings:
-    # 1. Store locally
-    # 2. Store in DB (for website) (only available if logged in)
-    # 3. Editable keybinds for taking screenshots
-    # 4. Stay logged in
-    settings: configparser.ConfigParser = configparser.ConfigParser()
-    settings_path = f'{AppDirs().settings_dir}\\settings.ini'
+    settings: configparser.ConfigParser
+    settings_path: str
+    storage_local: int 
+    storage_db: int 
+    is_private: int 
+    screen_shot: str 
+    screen_shot_edit: str 
+    settings_window: str 
     def __init__(self):
+        self.settings = configparser.ConfigParser()
+        self.settings_path = f'{AppDirs().settings_dir}/settings.ini'
         self.__generate_default_settings()
         self.__init_settings()
         self.__init_ui()
@@ -237,65 +254,67 @@ class SettingsWindow:
         self.__init_settings()
     
     def __init_settings(self):
-        self.storage_local: int = self.settings.getint('Storage', 'local')
-        self.storage_db: int = self.settings.getint('Storage', 'db')
-        self.is_private: int = self.settings.getint('Storage', 'is_private')
-        self.screen_shot: str = self.settings.get('KeyBinds', 'screen_shot')
-        self.screen_shot_edit: str = self.settings.get('KeyBinds', 'screen_shot_edit')
-        self.settings_window: str = self.settings.get('KeyBinds', 'settings_window')
+        self.storage_local = self.settings.getint('Storage', 'local')
+        self.storage_db = self.settings.getint('Storage', 'db')
+        self.is_private = self.settings.getint('Storage', 'is_private')
+        self.screen_shot = self.settings.get('KeyBinds', 'screen_shot')
+        self.screen_shot_edit = self.settings.get('KeyBinds', 'screen_shot_edit')
+        self.settings_window = self.settings.get('KeyBinds', 'settings_window')
     
     def __generate_default_settings(self):
-        if not os.path.exists(self.settings_path):
-            self.settings = configparser.ConfigParser()
-            self.settings['Storage'] = {'local': '1',
-                                        'db': '0',
-                                        'is_private': '1'}
-            self.settings['KeyBinds'] = {'screen_shot': 'ctrl+1',
-                                        'screen_shot_edit': 'ctrl+2',
-                                        'settings_window': 'ctrl+3'}
-            with open(self.settings_path, 'w') as settingsfile:
-                self.settings.write(settingsfile)
+        if os.path.exists(self.settings_path):
             self.settings.read(self.settings_path)
-        else:
-            self.settings.read(self.settings_path)
+            return
+        self.settings['Storage'] = {'local': '1',
+                                    'db': '0',
+                                    'is_private': '1'}
+        self.settings['KeyBinds'] = {'screen_shot': 'ctrl+1',
+                                    'screen_shot_edit': 'ctrl+2',
+                                    'settings_window': 'ctrl+3'}
+        with open(self.settings_path, 'w') as settingsfile:
+            self.settings.write(settingsfile)
+        self.settings.read(self.settings_path)
+
+class HotKey:
+    def __init__(self):
+        pass
     
-def main():
-    login_window = LoginWindow()
-    app_dirs = AppDirs()
-    if login_window.user.logged_in:
-        settings = SettingsWindow()
-    if login_window.user.logged_in:
-        def screen_shot(): # Full screen shot
-            screen = ImageGrab.grab()
-            image_name = f'\\{int(time.time())}.jpg'
-            image_path = app_dirs.images_dir + image_name
-            if settings.storage_local:
-                screen.save(image_path)
-            if settings.storage_db:
-                screen.save(image_path)
-                api = API()
-                with open(image_path, 'rb') as img:
-                    r_code = api.upload_img(login_window.user.username, login_window.user._password, bool(settings.is_private), GetWindowText(GetForegroundWindow()), img)
-                    img.close()
-                if not settings.storage_local:
-                    os.remove(image_path)
-        def screen_shot_edit(): # Select a rectangle inside screen shot
-            screen = ImageGrab.grab()
-            image_name = f'\\{int(time.time())}.jpg'
-            image_path = app_dirs.images_dir + image_name
-            EditableImageWindow(screen, image_path)
+    def screen_shot(self, app_dirs: AppDirs, settings: SettingsWindow, username, password): # Full screen shot
+        screen = ImageGrab.grab()
+        image_name = f'\\{int(time.time())}.jpg'
+        image_path = app_dirs.images_dir + image_name
+        if settings.storage_local:
+            screen.save(image_path)
+        if settings.storage_db:
+            screen.save(image_path)
             api = API()
             with open(image_path, 'rb') as img:
-                api.upload_img(login_window.user.username, login_window.user._password, bool(settings.is_private), GetWindowText(GetForegroundWindow()), img)
+                response = api.upload_img(username, password, bool(settings.is_private), GetWindowText(GetForegroundWindow()), img)
                 img.close()
             if not settings.storage_local:
                 os.remove(image_path)
-        def settings_window(): # Settings window
-            settings = SettingsWindow()
-            
-        keyboard.add_hotkey(settings.settings_window, settings_window)
-        keyboard.add_hotkey(settings.screen_shot_edit, screen_shot_edit)
-        keyboard.add_hotkey(settings.screen_shot, screen_shot)
+    
+    def screen_shot_edit(self, app_dirs: AppDirs, settings: SettingsWindow, username, password): # Select a rectangle inside screen shot
+        screen = ImageGrab.grab()
+        image_name = f'\\{int(time.time())}.jpg'
+        image_path = app_dirs.images_dir + image_name
+        EditableImageWindow(screen, image_path)
+        api = API()
+        with open(image_path, 'rb') as img:
+            api.upload_img(username, password, bool(settings.is_private), GetWindowText(GetForegroundWindow()), img)
+            img.close()
+        if not settings.storage_local:
+            os.remove(image_path)
+    
+def main():
+    login_window = LoginWindow()
+    if login_window.user.logged_in:
+        app_dirs = AppDirs()
+        settings = SettingsWindow()
+        hot_key = HotKey()
+        keyboard.add_hotkey(settings.settings_window, lambda: SettingsWindow())
+        keyboard.add_hotkey(settings.screen_shot_edit, lambda: hot_key.screen_shot_edit(app_dirs, settings, login_window.user.username, login_window.user._password))
+        keyboard.add_hotkey(settings.screen_shot, lambda: hot_key.screen_shot(app_dirs, settings, login_window.user.username, login_window.user._password))
         keyboard.wait()
     
 if __name__ == "__main__":
